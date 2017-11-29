c6e94daa5e5fa73c483969be759a93c521020ac9
Hans Predony <hans.predony@hotmail.com>
2017-11-29 16:21:04 +0100

    eddit note

--- kanban/src/components/App.jsx
+++ kanban/src/components/App.jsx
@@ -23,7 +23,12 @@ export default class App extends React.Component {
 
 		return (<div>
 			<button onClick={this.addNote}>+</button>
-			<Notes notes={notes}/>
+			<Notes
+          notes={notes}
+          onNoteClick={this.activateNoteEdit}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote}
+          />
 		</div>);
 	}
 	addNote = () => {
@@ -36,5 +41,34 @@ export default class App extends React.Component {
 			])
 		});
 	}
+	deleteNote = (id, e) => {
+		e.stopPropagation();
 
+		this.setState({
+			notes: this.state.notes.filter(note => note.id !== id)
+		});
+	}
+	activateNoteEdit = (id) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = true;
+        }
+
+        return note;
+      })
+    });
+  }
+  editNote = (id, task) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = false;
+          note.task = task;
+        }
+
+        return note;
+      })
+    });
+  }
 }
--- kanban/src/components/Notes.jsx
+++ kanban/src/components/Notes.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
-export default ({notes}) => (
+import Note from './Note';
+import Editable from './Editable';
 
-  <ul>{notes.map(note =>
-    <li key={note.id}>{note.task}</li>
-  )}</ul>
-);
+export default({
+	notes,
+	onNoteClick = () => {},
+	onEdit = () => {},
+	onDelete = () => {}
+}) => (<ul>{
+		notes.map(({id, editing, task}) => <li key={id}>
+			<Note onClick={onNoteClick.bind(null, id)}>
+				<Editable editing={editing} value={task} onEdit={onEdit.bind(null, id)}/>
+				<button onClick={onDelete.bind(null, id)}>x</button>
+			</Note>
+		</li>)
+	}</ul>)
