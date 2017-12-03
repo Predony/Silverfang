f66ff8e6b89aa830bbe855121428476cfae38998
Hans Predony <hans.predony@hotmail.com>
2017-12-04 00:50:47 +0100

    greetingpage die erste

--- kanban/src/components/App.jsx
+++ kanban/src/components/App.jsx
@@ -1,75 +1,10 @@
 import React from 'react';
-import uuid from 'uuid';
-import Notes from './Notes';
+import Greeting from './Greeting';
 import './App.css';
 
-export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			notes: [
-				{
-					id: uuid.v4(),
-					task: 'Learn React'
-				}, {
-					id: uuid.v4(),
-					task: 'Do laundry'
-				}
-			]
-		};
-	}
+class App extends React.Component {
 	render() {
-		const {notes} = this.state;
-
-		return (<div>
-			<button className="add-note" onClick={this.addNote}>+</button>
-			<Notes
-          notes={notes}
-          onNoteClick={this.activateNoteEdit}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote}
-          />
-		</div>);
+		return (<Greeting/>);
 	}
-	addNote = () => {
-		this.setState({
-			notes: this.state.notes.concat([
-				{
-					id: uuid.v4(),
-					task: 'New task'
-				}
-			])
-		});
-	}
-	deleteNote = (id, e) => {
-		e.stopPropagation();
-
-		this.setState({
-			notes: this.state.notes.filter(note => note.id !== id)
-		});
-	}
-	activateNoteEdit = (id) => {
-    this.setState({
-      notes: this.state.notes.map(note => {
-        if(note.id === id) {
-          note.editing = true;
-        }
-
-        return note;
-      })
-    });
-  }
-  editNote = (id, task) => {
-    this.setState({
-      notes: this.state.notes.map(note => {
-        if(note.id === id) {
-          note.editing = false;
-          note.task = task;
-        }
-
-        return note;
-      })
-    });
-  }
 }
+export default App
--- kanban/src/index.js
+++ kanban/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import Provider from './components/Provider';
 
 ReactDOM.render(
-  <App />,
+	<App />,
+
   document.getElementById('app')
 );
 registerServiceWorker();
